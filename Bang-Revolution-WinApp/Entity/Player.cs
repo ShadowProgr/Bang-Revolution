8877c2391b4f0885b103cac94a6717e51d2968db
tuanta1996 <tuanta04101996@gmail.com>
2016-08-11 15:26:29 +0700

    Add ConnectToDB and Entity
    * ConnectToDB:
    - Open DB
    - Check Login
    
    *Entity: Add BlueCard, BrownCard, Card, Character, Item, Loser, Winner,
    Player, Report, User class and interface Role that:
    - BlueCard and BrownCard extend Card
    - Player extend User and implement Role
    - Winner and Loser extend Player

---
+++ Bang-Revolution-WinApp/Entity/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class Player: User, Role
+    {
+        public int roleID { get; set; }
+        public string roleName { get; set; }
+        public string roleDesc { get; set; }
+        public string img { get; set; }
+        
+    }
+}
---
+++ Bang-Revolution-WinApp/Entity/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public interface Role
+    {
+        int roleID { get; set; }
+        string roleName { get; set; }
+        string roleDesc { get; set; }
+        string img { get; set; }
+    }
+}
