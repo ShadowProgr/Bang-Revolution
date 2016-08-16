06de6a8f98f09ee783efaa4d970368073ed65e9d
Hoàng Vương <shadowprogr@gmail.com>
2016-08-16 22:46:55 +0700

    Revert "Deleted Bang-Revolution-WebApp"
    This reverts commit 051137c9c358472c8dba190964068c446ea2bd5c.

---
+++ Bang-Revolution-WebApp/UserData/UData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class UData
+    {
+        public int id { get; set; }
+        public Int64 exp { get; set; }
+        public Int64 win { get; set; }
+        public Int64 lose { get; set; }
+        public float rate { get; set; }
+        public Int64 money { get; set; }
+    }
+}
