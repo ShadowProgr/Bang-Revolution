a4fbade05bab9b4ed805a1b6c32ac409c93c143c
Hoàng Vương <shadowprogr@gmail.com>
2016-08-16 22:41:40 +0700

    Created Bang-Revolution-Client
    Project created

---
+++ Bang-Revolution-Client/Bang-Revolution-Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bang_Revolution_Client
+{
+    static class Program
+    {
+        static bool loggedIn = false;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            if (!loggedIn)
+            {
+
+            }
+        }
+    }
+}
