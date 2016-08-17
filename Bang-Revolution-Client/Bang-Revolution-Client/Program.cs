e637a72dfc377080adf3a62d0760b8ed31457177
Mrdjay96 <Mrdjay96@gmail.com>
2016-08-17 11:17:49 +0700

    Tu s branch (#19)
    * update Web
    
    connect to DB server
    
    * MainServer
    
    Created MainServer
    
    * Added Cards' Images
    
    * Test
    
    * Fixed function names in GameEnvironment.cs
    
    - getCards to getCard
    - getChars to getCharbyUserID
    
    * Created Bang-Revolution-Client
    
    Project created
    
    * Updated Bang-Revolution-Client
    
    Minor updates
    
    * Updated Bang-Revolution-Client
    
    Created formMain
    
    * Chat - Server - Client
    
    Send data from client to server and to another clients
    
    * Updated Bang-Revolution-Client
    
    Added image load for formMain
    
    * Deleted Bang-Revolution-WebApp
    
    To fix sync issues
    
    * Merge remote-tracking branch 'refs/remotes/origin/master' into tu's-branch
    
    # Conflicts:
    #	Bang-Revolution-MainServer/Clients/Clients.csproj
    #	Bang-Revolution-MainServer/Clients/Properties/AssemblyInfo.cs
    #	Bang-Revolution-MainServer/DAL/DAL.csproj
    #	Bang-Revolution-MainServer/DAL/Properties/AssemblyInfo.cs
    #	Bang-Revolution-WebApp/Bang-Revolution-WebApp/Login.aspx.designer.cs
    #	Bang-Revolution-WebApp/Bang-Revolution-WebApp/Profile.aspx.designer.cs
    #	Bang-Revolution-WebApp/DAL/DAL.csproj
    #	Bang-Revolution-WebApp/DAL/Properties/AssemblyInfo.cs
    #	Bang-Revolution-WebApp/MainServer/MainServer.csproj
    #	Bang-Revolution-WebApp/MainServer/Properties/AssemblyInfo.cs

---
+++ Bang-Revolution-Client/Bang-Revolution-Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entity;
+
+namespace Bang_Revolution_Client
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+
+        public static List<Player> generateDummyData()
+        {
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 7; i++)
+            {
+                Role role = new Role()
+                {
+                    id = 1,
+                    name = "Sheriff",
+                    desc = "n/a",
+                    img = "sheriff.png"
+                };
+                Character character = new Character()
+                {
+                    id = 1,
+                    name = "Test char",
+                    desc = "n/a",
+                    hp = 5,
+                    skillID = 1,
+                    img = "char.png",
+                    price = 500
+                };
+                Player player = new Player()
+                {
+                    role = role,
+                    name = "player " + i,
+                    currentHP = character.hp,
+                    maxHP = character.hp,
+                    character = character,
+                    distance = 1,
+                    range = 1
+                };
+                players.Add(player);
+            }
+            return players;
+        }
+    }
+}
---
+++ Bang-Revolution-Client/Bang-Revolution-Client/frmLogin.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Bang_Revolution_Client
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(12, 102);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(258, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(89, 12);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(181, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(12, 15);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(89, 38);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(181, 20);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 41);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.Location = new System.Drawing.Point(12, 128);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(258, 23);
+            this.lnkRegister.TabIndex = 5;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Register";
+            this.lnkRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lnkRegister.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRegister_LinkClicked);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(15, 61);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblMessage.Size = new System.Drawing.Size(255, 38);
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.Text = "Message";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start Host";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(255, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Join Host";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 207);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lnkRegister);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnLogin);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.LinkLabel lnkRegister;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
