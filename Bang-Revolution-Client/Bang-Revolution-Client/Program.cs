a1453b7ed4331d61433fcfdd5826d5880cc3d5b6
Hoàng Vương <shadowprogr@gmail.com>
2016-08-17 12:06:08 +0700

    Shadowprogr s branch (#20)
    * Added Cards' Images
    
    * Fixed function names in GameEnvironment.cs
    
    - getCards to getCard
    - getChars to getCharbyUserID
    
    * Created Bang-Revolution-Client
    
    Project created
    
    * Updated Bang-Revolution-Client
    
    Minor updates
    
    * Updated Bang-Revolution-Client
    
    Created formMain
    
    * Updated Bang-Revolution-Client
    
    Added image load for formMain
    
    * Tu s branch (#17)
    
    * update Web
    
    connect to DB server
    
    * MainServer
    
    Created MainServer
    
    * Test
    
    * Chat - Server - Client
    
    Send data from client to server and to another clients
    
    * Revert 1
    
    * Deleted Bang-Revolution-WebApp
    
    To fix sync issues
    
    * Tu s branch (#16)
    
    * update Web
    
    connect to DB server
    
    * MainServer
    
    Created MainServer
    
    * Test
    
    * Chat - Server - Client
    
    Send data from client to server and to another clients
    
    * Deleted Bang-Revolution-WebApp
    
    To fix sync issues
    
    * Deleted Bang-Revolution-WebApp again
    
    To prevent sync issues
    
    * Merge remote-tracking branch 'refs/remotes/origin/master' into tu's-branch
    
    # Conflicts:
    #	Bang-Revolution-MainServer/Clients/Clients.csproj
    #	Bang-Revolution-MainServer/Clients/Properties/AssemblyInfo.cs
    #	Bang-Revolution-MainServer/DAL/DAL.csproj
    #	Bang-Revolution-MainServer/DAL/Properties/AssemblyInfo.cs
    #	Bang-Revolution-WebApp/Bang-Revolution-WebApp/Login.aspx.designer.cs
    #	Bang-Revolution-WebApp/Bang-Revolution-WebApp/Profile.aspx.designer.cs
    #	Bang-Revolution-WebApp/DAL/DAL.csproj
    #	Bang-Revolution-WebApp/DAL/Properties/AssemblyInfo.cs
    #	Bang-Revolution-WebApp/MainServer/MainServer.csproj
    #	Bang-Revolution-WebApp/MainServer/Properties/AssemblyInfo.cs
    
    * Revert "Tu s branch" (#18)
    
    * Finished client side code for Bang-Revolution-Client
    
    * 1
    
    * 123
    
    * Merge remote-tracking branch 'refs/remotes/origin/master' into shadowprogr's-branch
    
    # Conflicts:
    #	Bang-Revolution-Client/Bang-Revolution-Client/Resources/Characters/Sid
    Ketchum.png
    #	Images/Characters/Sid  Ketchum.png
    #	Images/Characters/Sid Ketchum.png
    
    * 1

---
+++ Bang-Revolution-Client/Bang-Revolution-Client/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entity;
+
+namespace Bang_Revolution_Client
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+
+        public static List<Player> generateDummyData()
+        {
+            List<Card> cards = new List<Card>();
+            Card card = new Card()
+            {
+                id = 9,
+                name = "Bang",
+                type = 0,
+                img = "Playable Cards\\Bang C2.png",
+                effectID = 6,
+                rank = 2,
+                suit = 1
+            };
+            cards.Add(card);
+            cards.Add(card);
+            Card card2 = new Card()
+            {
+                id = 10,
+                name = "Playable Cards\\Panic D8.png",
+                type = 0,
+                img = "Playable Cards\\Panic D8.png",
+                effectID = 9,
+                rank = 2,
+                suit = 1
+            };
+            cards.Add(card2);
+            Card card3 = new Card()
+            {
+                id = 10,
+                name = "Playable Cards\\Panic D8.png",
+                type = 0,
+                img = "Playable Cards\\Beer H6.png",
+                effectID = 11,
+                rank = 2,
+                suit = 1
+            };
+            cards.Add(card3);
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < 7; i++)
+            {
+                Role role = new Role()
+                {
+                    id = 1,
+                    name = "Sheriff",
+                    desc = "n/a",
+                    img = "sheriff.png"
+                };
+                Character character = new Character()
+                {
+                    id = 1,
+                    name = "Test char",
+                    desc = "n/a",
+                    hp = 5,
+                    skillID = 1,
+                    img = "Characters\\El Gringo.png",
+                    price = 500
+                };
+                Player player = new Player()
+                {
+                    cardsOnHand = cards,
+                    id = i,
+                    role = role,
+                    name = "player " + i,
+                    currentHP = character.hp,
+                    maxHP = character.hp,
+                    character = character,
+                    distance = 0,
+                    range = 2,
+                    scope = 1,
+                    usedBang = false
+                };
+                players.Add(player);
+            }
+            return players;
+        }
+    }
+}
---
+++ Bang-Revolution-Client/Bang-Revolution-Client/frmLogin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entity;
+
+namespace Bang_Revolution_Client
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+            lblMessage.Text = "";
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // Send login info to server and wait for reply
+            if (true) // If login succeeded
+            {
+                frmMain formMain = new frmMain();
+                formMain.Show();
+                this.Hide();
+            } else
+            {
+                lblMessage.Text = "Wrong username/password";
+            }
+            
+        }
+
+        private void lnkRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
